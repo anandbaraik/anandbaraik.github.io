3302b0cc10603ede5cabd897581daeab2fab7b96
anandbaraik <anandbaraik2014@gmail.com>
2022-04-20 23:04:00 +0530

    routes & feature folder structured

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,22 +1,27 @@
 import React from 'react';
-import './App.css';
+import "./App.css";
+
+import {BrowserRouter, Route} from "react-router-dom";
+
+import Home from "./../Home/Home.jsx";
+import About from "./../About/About.jsx";
+import Projects from "./../Projects/Projects.jsx";
+import Skills from "./../Skills/Skills.jsx";
+import Contact from "./../Contact/Contact.jsx";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Portfolio in making using <code>React.js</code>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          In progress
-        </a>
-      </header>
+      <h1>I'm Landing page</h1>
+      <BrowserRouter>
+        <div>
+          <Route path="/" exact component={Home}></Route>
+          <Route path="/about" exact component={About}></Route>
+          <Route path="/projects" exact component={Projects}></Route>
+          <Route path="/skills" exact component={Skills}></Route>
+          <Route path="/contact" exact component={Contact}></Route>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
