8a47c7e05bf4e04197db65ae13e3209a08806f6e
anandbaraik <anandbaraik2014@gmail.com>
2022-04-23 23:06:42 +0530

    navbar component added

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import {HashRouter, Route} from "react-router-dom";
 
+import NavBar from "./../NavBar/NavBar.jsx";
 import Home from "./../Home/Home.jsx";
 import About from "./../About/About.jsx";
 import Projects from "./../Projects/Projects.jsx";
@@ -13,6 +14,7 @@ function App() {
     <div>
       <HashRouter>
         <div>
+          <NavBar/>
           <Route path="/" exact component={Home}/>
           <Route path="/about" exact component={About}/>
           <Route path="/projects" exact component={Projects}/>
