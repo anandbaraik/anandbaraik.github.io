208895fb4802d64c54e1c865835372ad53cb1da4
anandbaraik <anandbaraik2014@gmail.com>
2022-05-20 22:54:06 +0530

    update:social icon

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -3,6 +3,12 @@ import "./Contact.css";
 import Header from "./../Header/Header.jsx"
 import Footer from "./../Footer/Footer.jsx"
 import contactVector from "./../../assets/contact_anime.png";
+import gitHub from "./../../assets/gh.png";
+import instaGram from "./../../assets/in.png";
+import linkedIn from "./../../assets/li.png";
+import medium from "./../../assets/me.png";
+import web from "./../../assets/web.png";
+
 function Contact() {
   return (
     <div className='section-container'>
@@ -16,6 +22,33 @@ function Contact() {
           <textarea type="text" placeholder='Message' name='' className='input-box body-input'></textarea>
           <button type="submit" class="contact-btn">Send Email</button>
         </form>
+      </div>
+      <div className='social-icons-container'>
+        <a href="https://github.com/anandbaraik"
+          className='social-icon'
+          target="_blank">
+          <img src={gitHub} alt='github'/>
+        </a>
+        <a href="https://www.linkedin.com/in/anand-baraik/" 
+          className='social-icon'
+          target="_blank">
+          <img src={linkedIn} alt='linkedin'/>
+        </a>
+        <a href="https://peerlist.io/anandbaraik" 
+          className='social-icon'
+          target="_blank">
+          <img src={web} alt='web'/>
+        </a>
+        <a href="https://medium.com/@anandbaraik"
+          className='social-icon'
+          target="_blank">
+          <img src={medium} alt='medium'/>
+        </a>
+        <a href="https://www.instagram.com/anand__babu/"
+          className='social-icon'
+          target="_blank">
+          <img src={instaGram} alt='instagram'/>
+        </a>
       </div>
       <Footer 
         phrase="Read more "
