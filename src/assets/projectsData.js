3cd8991dba374cf7882832f68f2da9d28b56ad49
anandbaraik <anandbaraik2014@gmail.com>
2022-05-27 05:53:33 +0530

    update:project url

--- src/assets/projectsData.js
+++ src/assets/projectsData.js
@@ -32,6 +32,6 @@ export const projectsData = [
         projectDescription:
             'As a developer, we should definitely have our own portfolio website. There are endless benefits of it. so here is one of me!',
         imageUrl: 'https://user-images.githubusercontent.com/31516195/170092193-d24327ca-f035-4074-8fde-57238ef570d6.png',
-        projectUrl: 'https://anandbaraik.github.io/react-portfolio/',
+        projectUrl: 'https://anandbaraik.github.io',
     },
 ];
